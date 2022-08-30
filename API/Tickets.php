1c4fbc483661af2e3008e7559f0f9e0a34b80c71
vipin shrivastava <vipin.shrivastava889@webkul.com>
2022-08-30 12:36:23 +0530

    remove unused code

--- API/Tickets.php
+++ API/Tickets.php
@@ -70,10 +70,6 @@ class Tickets extends AbstractController
             }
         }
 
-        if ($request->query->get('limit')) {
-            $request->query->set('limit', $request->query->get('limit'));
-        }
-
         $json = $ticketRepository->getAllTickets($request->query, $container);
 
         $json['userDetails'] = [
